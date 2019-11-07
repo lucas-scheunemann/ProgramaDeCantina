770998edc691f67ebc71520b4a7bd45ee7f76659
Lucas Scheunemann <l182441@g.unicamp.br>
2019-11-07 16:01:11 -0300

    inserção do projeto no GitHub
    Esse projeto é uma forma de visualização de uma tela do recepcionista de uma cantina, com uma lista finita e definida de opções no menu, somando o preço total e exibindo na tela.

---
+++ lucas/Sistema_cantina/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Sistema_cantina
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtCodigo = new System.Windows.Forms.TextBox();
+			this.lblTitulo = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lblTotal = new System.Windows.Forms.Label();
+			this.lstCaixa = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// txtCodigo
+			// 
+			this.txtCodigo.Location = new System.Drawing.Point(29, 67);
+			this.txtCodigo.Name = "txtCodigo";
+			this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+			this.txtCodigo.TabIndex = 1;
+			this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+			// 
+			// lblTitulo
+			// 
+			this.lblTitulo.AutoSize = true;
+			this.lblTitulo.Location = new System.Drawing.Point(132, 9);
+			this.lblTitulo.Name = "lblTitulo";
+			this.lblTitulo.Size = new System.Drawing.Size(83, 13);
+			this.lblTitulo.TabIndex = 2;
+			this.lblTitulo.Text = "Sistema Cantina";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(35, 51);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(94, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Código do produto";
+			// 
+			// lblTotal
+			// 
+			this.lblTotal.AutoSize = true;
+			this.lblTotal.Location = new System.Drawing.Point(68, 228);
+			this.lblTotal.Name = "lblTotal";
+			this.lblTotal.Size = new System.Drawing.Size(61, 13);
+			this.lblTotal.TabIndex = 4;
+			this.lblTotal.Text = "Valor Total:";
+			// 
+			// lstCaixa
+			// 
+			this.lstCaixa.FormattingEnabled = true;
+			this.lstCaixa.Location = new System.Drawing.Point(135, 51);
+			this.lstCaixa.Name = "lstCaixa";
+			this.lstCaixa.Size = new System.Drawing.Size(231, 173);
+			this.lstCaixa.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(442, 310);
+			this.Controls.Add(this.lstCaixa);
+			this.Controls.Add(this.lblTotal);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.lblTitulo);
+			this.Controls.Add(this.txtCodigo);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox txtCodigo;
+		private System.Windows.Forms.Label lblTitulo;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblTotal;
+		private System.Windows.Forms.ListBox lstCaixa;
+	}
+}
+
---
+++ lucas/Sistema_cantina/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_cantina
+{
+	public partial class Form1 : Form
+	{
+
+		string[] produtos = new string[6];
+		string[] codigo = new string[6];
+		double[] valor = new double[6];
+		double soma;
+		
+			
+
+
+		public Form1()
+		{
+			InitializeComponent();
+			codigo[1] = "001";
+			codigo[2] = "002";
+			codigo[3] = "003";
+			codigo[4] = "004";
+			codigo[5] = "005";
+			produtos[1] = "Pastel";
+			produtos[2] = "Coxinha";
+			produtos[3] = "Hot_Dog";
+			produtos[4] = "Chocolate";
+			produtos[5] = "Suco";
+			valor[1] = 6.00;
+			valor[2] = 5.00;
+			valor[3] = 12.00;
+			valor[4] = 3.50;
+			valor[5] = 8.00;
+		}
+
+		private void txtCodigo_TextChanged(object sender, EventArgs e)
+		{
+			if (txtCodigo.Text.Length == 3)
+			/*inicia com uma condição, se o usuário digitar um código com 5
+			caracteres (length),
+			esse código é adicionado ao listBox.
+			A propriedade length retorna a quantidade de caracteres.*/
+			{
+				int indice = 0;
+				for (int prod = 1; prod < codigo.Length; prod++)
+				/*Faça enquanto o prod for menor ou igual a qtde de itens do array
+				*/
+				{
+					if (txtCodigo.Text == codigo[prod])
+					{
+						indice = prod;
+					}
+				}
+				if (indice == 0)
+				{
+					MessageBox.Show("Produto não encontrado");
+				}
+				else
+				{
+					lstCaixa.Items.Add(txtCodigo.Text + " -- " + produtos[indice]
+					+ "-- R$ " + valor[indice]);
+					/* Conteúdo adicionado no ListBox, concatena (+) as
+					informaçôes (código/nome/valor)*/
+					soma = soma + valor[indice];
+					lblTotal.Text = ("Valor Total R$ " + soma);
+					txtCodigo.Text = "";
+					txtCodigo.Focus();
+					/*localização da imagem que deve ser colocada em uma pasta em
+					c: em uma pasta imagens
+					o textbox será limpo e o Focus posiciona o cursor
+					para o TextBox para uma nova digitação*/
+				}
+			}
+		}
+	}
+}
